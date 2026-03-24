b803b230566d41b4b9bf24a17b05b842913d0bdf
Epalzeo <epalzeo.lithe@gmail.com>
2026-03-24 21:02:16 +0100

    1.8.23 YUV widget done

--- ABView 1.8.py
+++ ABView 1.8.py
@@ -46,7 +46,7 @@ class VideoYUVOpenGLWidget(QOpenGLWidget):
             layout(location = 0) in vec2 position;
             out vec2 texCoord;
             void main() {
-                texCoord = (position + 1.0) / 2.0;
+                texCoord = vec2((position.x + 1.0) / 2.0, 1.0 - (position.y + 1.0) / 2.0);
                 gl_Position = vec4(position, 0.0, 1.0);
             }
         """)
